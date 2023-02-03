cb34c2b52cc2ac8698deb7ac21568fa94e8fd681
vini.wessner@gmail.com <Capitao@caverna55>
2023-02-03 14:17:51 -0300

    Aprendendo sobre coleções

---
+++ src/main/kotlin/Colecoes/main.kt
@@ -0,0 +1,35 @@
+package Colecoes
+
+fun main() {
+    //list off,nao conseguimos alterar ela ou modificar
+    val lista1 = listOf(12, 10, 9, 8, 9, 4)
+
+    //nao conseguimos modificar ela, é uma lista que conseguimos alterar
+   val lista2 = mutableListOf(1,50,30,45,90,)
+
+
+    println(lista2[0])
+    println(lista2.size)
+    println(lista2)
+    println("--- Adicionando um valor ----")
+    lista2.add(40)
+    println(lista2)
+    println("--- removendo um valor ----")
+    lista2.remove(1)
+    println(lista2)
+    println("--- removendo que esta na primeira possicao ----")
+    lista2.removeAt(0)
+    println(lista2)
+
+    println("--- verificando se contem o valor 45 ----")
+    println( lista2.contains(45))
+
+
+
+
+
+
+
+
+
+}
